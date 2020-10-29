e1e74fc4edc27873c70fc0e367b63d62afe116e7
tyrantype <allvengeance6661@gmail.com>
2020-10-29 09:04:04 +0700

    RWD topnav (petugas)

--- petugas/header.php
+++ petugas/header.php
@@ -55,18 +55,32 @@
 <html>
     <head>
         <title>Halaman Petugas</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="../style/style.css">
         <style type="text/css">
                 li {margin-right: 10px;}
         </style>
     </head>
 <body>
-    <div>
-        <div class="navbar" id="navbar">
-            <!-- <li><b>Aplikasi Pembayaran SPP</b></li> -->
-            <a href="?p=transaksi">Transaksi</a>
-            <a href="?p=siswa">Data Siswa</a>
-            <a id="logout" href="?p=logout">Logout</a>
-        </div>
+    <div class="topnav" id="myTopnav">
+        <!-- <a>Aplikasi Pembayaran SPP</a> -->
+        <a href="?p=transaksi">Transaksi</a>
+        <a href="?p=siswa">Data Siswa</a>
+        <a id="logout" href="?p=logout">Logout</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
     </div>
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+    </script>
+    
     
