de3233a241a74918011701a7595a1c5e11e9dcc9
tyrantype <allvengeance6661@gmail.com>
2020-10-29 08:57:42 +0700

    tambah RWD topnav

--- includes/admin/header.php
+++ includes/admin/header.php
@@ -18,14 +18,16 @@ session_start();
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="../../style/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <title>Halaman Admin</title>
         <style type="text/css">
             li {margin-right: 10px;}
         </style>
     </head>
 <body>
-    <div class="navbar">
+    <div class="topnav" id="myTopnav">
         <a href="?p=beranda">Beranda</a>
         <a href="?p=siswa">Data Siswa</a>
         <a href="?p=petugas">Data Petugas</a>
@@ -33,4 +35,17 @@ session_start();
         <a href="?p=laporan">Laporan</a> 
         <!-- <a href="?p=generate">Generate Data Siswa</a>  -->
         <a id="logout" href="?p=logout">Logout</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
     </div>
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+    </script>
